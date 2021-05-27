81c0e38bfde96271962f8e3fa2568186769c870c
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2021-05-27 20:44:03 -0300

    menu button effect full implemented

--- components/menu.js
+++ components/menu.js
@@ -1,28 +1,60 @@
 const editorCodigoLabel = document.querySelector('[data-label-editor-codigo]');
 const paragrafo = editorCodigoLabel.querySelector('[data-p-editor-codigo]');
-const linkImg = editorCodigoLabel.querySelector('[data-img-editor-codigo]');
+const divImg = editorCodigoLabel.querySelector('[data-div-editor-codigo]');
 
-editorCodigoLabel.addEventListener("mouseover", () => {
-
+paragrafo.addEventListener("mouseover", () => {
+    
     const paragrafoStyle = paragrafo.style;
-    const linkImgStyle = linkImg.style;
 
+    const linkImgStyle = divImg.style;
+
+    paragrafoStyle.cursor = "pointer";
     paragrafoStyle.transition = '1s';
     paragrafoStyle.opacity = 0.5;
-    
+
+
     linkImgStyle.transition = '1s';
     linkImgStyle.backgroundColor = '#355DB6';
-
 });
 
-editorCodigoLabel.addEventListener("mouseout", () => {
-
+paragrafo.addEventListener("mouseout", () => {
+    
     const paragrafoStyle = paragrafo.style;
-    const linkImgStyle = linkImg.style;
+
+    const linkImgStyle = divImg.style;
 
     paragrafoStyle.transition = '1s';
     paragrafoStyle.opacity = 1;
-    
+
+
     linkImgStyle.transition = '1s';
-    linkImgStyle.backgroundColor = 'transparent';    
+    linkImgStyle.backgroundColor = 'transparent';
+});
+
+divImg.addEventListener("mouseover", () => {
+    
+    const divImgStyle = divImg.style;
+    const linkImg = divImg.querySelector('[data-img-editor-codigo]');
+    const linkImgStyle = linkImg.style;
+
+    const paragrafoStyle = paragrafo.style;
+
+    linkImgStyle.cursor = "pointer";
+
+    divImgStyle.transition = '1s';
+    divImgStyle.backgroundColor = '#355DB6';
+
+    paragrafoStyle.opacity = 0.5;
+});
+
+divImg.addEventListener("mouseout", () => {
+    
+    const divImgStyle = divImg.style;
+
+    const paragrafoStyle = paragrafo.style;
+
+    divImgStyle.transition = '1s';
+    divImgStyle.backgroundColor = 'transparent';
+
+    paragrafoStyle.opacity = 1;
 });
