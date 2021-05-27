ffb66b87427e011b414bdbda6f1b65cc8664e84e
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2021-05-27 15:15:54 -0300

    Editor de codigo button efect created

---
+++ components/menu.js
@@ -0,0 +1,28 @@
+const editorCodigoLabel = document.querySelector('[data-label-menu]');
+const paragrafo = editorCodigoLabel.querySelector('[data-p-editor-codigo]');
+const linkImg = editorCodigoLabel.querySelector('[data-img-editor-codigo]');
+
+editorCodigoLabel.addEventListener("mouseover", () => {
+
+    const paragrafoStyle = paragrafo.style;
+    const linkImgStyle = linkImg.style;
+
+    paragrafoStyle.transition = '1s';
+    paragrafoStyle.opacity = 0.5;
+    
+    linkImgStyle.transition = '1s';
+    linkImgStyle.backgroundColor = '#355DB6';
+
+});
+
+editorCodigoLabel.addEventListener("mouseout", () => {
+
+    const paragrafoStyle = paragrafo.style;
+    const linkImgStyle = linkImg.style;
+
+    paragrafoStyle.transition = '1s';
+    paragrafoStyle.opacity = 1;
+    
+    linkImgStyle.transition = '1s';
+    linkImgStyle.backgroundColor = 'transparent';    
+});
