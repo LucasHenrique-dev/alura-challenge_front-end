1eabe7346e5dadd32fd32a31b8779e7d3a14fefe
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2021-06-02 14:54:09 -0300

    highlight syntax implemented

--- scripts/editorCodigo.js
+++ scripts/editorCodigo.js
@@ -1,18 +1,20 @@
-(() => {const colorBackground = document.querySelector('[data-color-background]');
-const colorPicker = document.querySelector('[data-personalizar-cor]');
+(() => {
+    const colorBackground = document.querySelector('[data-color-background]');
+    const colorPicker = document.querySelector('[data-personalizar-cor]');
 
-colorPicker.addEventListener("input", updateFirst, false);
-colorPicker.addEventListener("change", watchColorPicker, false);
+    colorPicker.addEventListener("input", updateFirst, false);
+    colorPicker.addEventListener("change", watchColorPicker, false);
 
-function updateFirst(event) {    
-    colorBackground.style.background = event.target.value;
-}
+    function updateFirst(event) {    
+        colorBackground.style.background = event.target.value;
+    }
 
-function watchColorPicker() {
+    function watchColorPicker() {
 
-    const colorBackgroundStyle = colorBackground.style;
+        const colorBackgroundStyle = colorBackground.style;
 
-    colorBackgroundStyle.animation = "pulse 2s infinite";
+        colorBackgroundStyle.animation = "pulse 2s infinite";
 
-    setTimeout(() => colorBackgroundStyle.animation = "", 1000);
-}})()
+        setTimeout(() => colorBackgroundStyle.animation = "", 1000);
+    }
+})()
--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,11 +1,18 @@
-const areaDocodigo = document.querySelector('[data-code-area]');
+const areaDoCodigo = document.querySelector('[data-code-area]');
 const linguagem = document.querySelector('[data-language-selection]');
 const botaoHighlight = document.querySelector('[data-highlight-button]');
 
 function mudaLinguagem() {
+
     const codigo = areaDoCodigo.querySelector('code');
-    areaDocodigo.innerHTML = `<code class="code-campo-codigo hljs ${linguagem.value}" contenteditable="true" aria-label="editor"></code>`;
-    areaDocodigo.firstChild.innerText = codigo.innerText;
+    const conteudoCodigo = codigo.innerHTML;
+
+    areaDoCodigo.innerHTML = `
+    <div>
+        <img src="/assets/Images/mac_buttons.svg" alt="Representação dos Botões de um terminal Mac">
+    </div>
+    <code class="code-campo-codigo hljs ${linguagem.value}" contenteditable="true" aria-label="editor">${conteudoCodigo}</code>`;
+    areaDoCodigo.firstChild.innerText = codigo.innerText;
 }
 
 linguagem.addEventListener('change', () => {
@@ -13,6 +20,10 @@ linguagem.addEventListener('change', () => {
 })
 
 botaoHighlight.addEventListener('click', () => {
-    const codigo = areaDocodigo.querySelector('code');
+
+    const codigo = areaDoCodigo.querySelector('code');
+
+    console.log(hljs);
+
     hljs.highlightBlock(codigo);
 });
