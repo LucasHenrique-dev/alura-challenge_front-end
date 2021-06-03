938af7bf1c145638c5758a504b394ec70d98286b
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2021-06-02 23:01:03 -0300

    code-content scroll implemented

--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,29 +1,41 @@
-const areaDoCodigo = document.querySelector('[data-code-area]');
-const linguagem = document.querySelector('[data-language-selection]');
-const botaoHighlight = document.querySelector('[data-highlight-button]');
+(() => {
+    const areaDoCodigo = document.querySelector('[data-code-area]');
+    const linguagem = document.querySelector('[data-language-selection]');
+    const botaoHighlight = document.querySelector('[data-highlight-button]');
 
-function mudaLinguagem() {
+    function mudaLinguagem() {
+        
+        const codigo = areaDoCodigo.querySelector('code');
+        const conteudoCodigo = codigo.innerHTML;
+        
+        areaDoCodigo.innerHTML = `
+        <div>
+            <img src="/assets/Images/mac_buttons.svg" alt="Representação dos Botões de um terminal Mac">
+        </div>
+        <pre class="code-content" data-code-content>
+            <code class="code-campo-codigo hljs ${linguagem.value}" contenteditable="true" aria-label="editor">${conteudoCodigo}</code>
+        </pre>`;
 
-    const codigo = areaDoCodigo.querySelector('code');
-    const conteudoCodigo = codigo.innerHTML;
+        const paiCodigo = document.querySelector('[data-code-content]');
+        const paiCodigoStyle = paiCodigo.style;
 
-    areaDoCodigo.innerHTML = `
-    <div>
-        <img src="/assets/Images/mac_buttons.svg" alt="Representação dos Botões de um terminal Mac">
-    </div>
-    <code class="code-campo-codigo hljs ${linguagem.value}" contenteditable="true" aria-label="editor">${conteudoCodigo}</code>`;
-    areaDoCodigo.firstChild.innerText = codigo.innerText;
-}
+        areaDoCodigo.firstChild.innerText = codigo.innerText;
+        paiCodigoStyle.overflow = "hidden";
+    }
 
-linguagem.addEventListener('change', () => {
-    mudaLinguagem();
-})
+    linguagem.addEventListener('change', () => {
+        mudaLinguagem();
+    })
 
-botaoHighlight.addEventListener('click', () => {
+    botaoHighlight.addEventListener('click', () => {
 
-    const codigo = areaDoCodigo.querySelector('code');
+        const codigo = areaDoCodigo.querySelector('code');
+        const paiCodigo = codigo.parentElement;
+        const paiCodigoStyle = paiCodigo.style;
 
-    console.log(hljs);
+        paiCodigoStyle.overflow = "hidden";
+        console.log(paiCodigoStyle);
 
-    hljs.highlightBlock(codigo);
-});
+        hljs.highlightBlock(codigo);
+    });
+})()
